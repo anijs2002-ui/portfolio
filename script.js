014b92cbd7f6f2c8780f4836afe0a0d7c8905e44
anijs2002-ui <anijs2002@gmail.com>
2026-03-17 11:41:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const sideMenu=document.querySelector("#sideMenu");
+const navbar=document.querySelector("nav");
+const navLinks=document.querySelector("nav ul");
+
+function openMenu(){
+    sideMenu.style.transform='translateX(-16rem)';
+}
+function closeMenu(){
+    sideMenu.style.transform='translateX(16rem)';
+}
+window.addEventListener('scroll',()=>{
+    if(scrollY>50){
+        navbar.classList.add('bg-white','bg-opacity-50','backdrop-blur-lg',
+            'shadow-sm','dark:bg-darkTheme','dark:shadow-white/20'
+        );
+        navLinks.classList.remove('bg-white','shadow-sm','bg-opacity-50','dark:border','dark:border-white/50','dark:bg-transparent');
+    }else{
+        navbar.classList.remove('bg-white','bg-opacity-50','backdrop-blur-lg',
+            'shadow-sm','dark:bg-darkTheme','dark:shadow-white/20'
+        );
+        navLinks.classList.add('bg-white','shadow-sm','bg-opacity-50','dark:border','dark:border-white/50','dark:bg-transparent');
+    }
+})
+// light mode and dark mode toggle
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+);
+
+function toggleTheme(){
+    document.documentElement.classList.toggle("dark");
+    if(document.documentElement.classList.contains("dark")){
+        localStorage.theme="dark";
+    } else {
+        localStorage.theme="light";
+    }
+}
+
+function toggleService(card) {
+    const moreText = card.querySelector(".service-more");
+
+    moreText.classList.toggle("hidden");
+}
